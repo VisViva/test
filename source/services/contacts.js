633f37f2dd72d35bcc5337aca549df673ad2f88f
Vis Viva <a.vis.viva@gmail.com>
2017-04-21 03:10:43 +0300

    Started implementing all contacts

--- source/services/contacts.js
+++ source/services/contacts.js
@@ -3,9 +3,77 @@
  */
 
 export class ContactsService {
-    constructor($http, StoreService) {
+    constructor($http, $state, StoreService) {
         'ngInject';
 
         this._$http = $http;
+        this._$state = $state;
+        this._StoreService = StoreService;
+    }
+
+    getAllContacts() {
+        // return this._$http({
+        //     url: '/api/contacts_list',
+        //     method: 'GET',
+        //     headers: {
+        //         'Authorization': this._StoreService.getToken()
+        //     }
+        // }).then(
+        //     response => {
+        //         if (response.data.success) {
+        //             this._StoreService.setAllContacts(response.data.data);
+        //         } else {
+        //             this._StoreService.setAllContacts([]);
+        //         }
+        //     },
+        //     error => {
+        //         switch (error.status) {
+        //             case 400:
+        //                 break;
+        //             case 404:
+        //                 this._StoreService.setModal(true);
+        //                 break;
+        //         }
+        //         this._StoreService.setAllContacts([]);
+        //     }
+        // );
+
+        this._StoreService.setAllContacts(
+            [{
+                contacts: [{
+                    id: 0,
+                    text: 'email0@gmail.com',
+                    type: 'email'
+                }, {
+                    id: 1,
+                    text: 'email1@gmail.com',
+                    type: 'email'
+                }, {
+                    id: 2,
+                    text: 'email2@gmail.com',
+                    type: 'email'
+                }, ],
+                id: 0,
+                name: 'John',
+                surname: 'Smith'
+            }, {
+                contacts: [{
+                    id: 3,
+                    text: 'email3@gmail.com',
+                    type: 'email'
+                }, {
+                    id: 4,
+                    text: 'email4@gmail.com',
+                    type: 'email'
+                }, {
+                    id: 5,
+                    text: 'email5@gmail.com',
+                    type: 'email'
+                }, ],
+                id: 1,
+                name: 'Jack',
+                surname: 'Black'
+            }]
+        );
     }
 }
--- source/views/all/all.controller.js
+++ source/views/all/all.controller.js
@@ -1,6 +1,24 @@
 class AllController {
-    constructor(StoreService) {
+    constructor(ContactsService, StoreService) {
         "ngInject";
+
+        this.allContacts = [];
+
+        this._ContactsService = ContactsService;
+        this._StoreService = StoreService;
+    }
+
+    handler = (allContacts) => {
+        this.allContacts = allContacts;
+    };
+
+    $onInit = function() {
+        this._StoreService.subscribe('allContacts', this.handler);
+        this._ContactsService.getAllContacts();
+    }
+
+    $onDestroy = function() {
+        this._StoreService.unSubscribe('allContacts', this.handler);
     }
 }
 
